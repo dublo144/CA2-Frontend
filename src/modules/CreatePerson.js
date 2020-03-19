9e91b8b864165aba470396cf92d5284403e68014
Mads Brandt <mads.brandt93@gmail.com>
2020-03-19 23:54:53 +0100

    added person cards and create person

--- src/modules/CreatePerson.js
+++ src/modules/CreatePerson.js
@@ -36,4 +36,5 @@ const formatPersonObject = person => {
       }
     ]
   };
+  return formattedPerson;
 };
--- src/utils/htmlOperations.js
+++ src/utils/htmlOperations.js
@@ -1,25 +1,20 @@
-export const printTableArray = array => {
-  let table = "<table align='center'>";
-  table += '<tr>';
-  for (let key of Object.keys(array[0])) {
-    table += `<th id=${key}> ${key} </th>`;
-  }
-  table += '</tr>';
-  array.forEach(member => {
-    table += '<tr>';
-    for (let values of Object.values(member)) {
-      table += `<td> ${values} </td>`;
-    }
-    table += '</tr>';
-  });
-  table += '</table>';
-  return table;
-};
-
 export const printCards = array => {
   const cardsMap = array.map(person => {
+    const firstName = nullCheck(person.firstName);
+    const lastName = nullCheck(person.lastName);
+    const email = nullCheck(person.email);
+    const street = nullCheck(person.address.street);
+    const additionalInfo = nullCheck(person.address.additionalInfo);
+    const zip = nullCheck(person.address.cityInfo.zipCode);
+    const city = nullCheck(person.address.cityInfo.city);
+
     let cardBody = `<div class='card-body'>
-        <h5 class='card-title'>${person.firstName} ${person.lastName}</h5>
+        <h5 class='card-title'>${firstName} ${lastName}</h5>
+        <p class="card-text"><b>Email: </b>${email}</p>
+        <p class="card-text"><b>Address: </b>${street} ${zip} ${city}</p>
+        <p class="card-text"><b>Additional Info: </b>${additionalInfo}</p>
+        ${handleHobbies(person.hobbies)}
+        ${handlePhones(person.phones)}
       </div>`;
     let card = `<div class='card' style='width: 18rem;>
         ${cardBody}
@@ -28,3 +23,27 @@ export const printCards = array => {
   });
   return cardsMap.join('');
 };
+
+const handleHobbies = hobbies => {
+  if (Array.isArray(hobbies) && hobbies.length) {
+    return hobbies.map(hobby => {
+      `<p><b>Name: </b>${hobby.name}</p>
+      <p><b>Description: </b>${hobby.description}</p>`;
+    });
+  } else {
+    return `<h6>No hobbies</h6>`;
+  }
+};
+
+const handlePhones = phones => {
+  if (Array.isArray(phones) && phones.length) {
+    return phones.map(phone => {
+      `<p><b>Phone: </b>${phone.number}</p>
+      <p><b>Description: </b>${phone.description}</p>`;
+    });
+  } else {
+    return `<h6>No phones</h6>`;
+  }
+};
+
+const nullCheck = property => (property ? property : '');
